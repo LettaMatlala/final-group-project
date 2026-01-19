b2a3e6c945162bfe55928e770ac7850f9312a42d
selinamalatji <selina.malatji@ul.ac.za>
2026-01-19 12:28:29 +0200

    COMMENT AND STUDENT OBJECT

--- group 1 project/Program.cs
+++ group 1 project/Program.cs
@@ -10,15 +10,36 @@ namespace group_1_project
     {
         static void Main(string[] args)
         {
+            // USER NAME
             Console.WriteLine("Enter your Name");
-
             string name = Convert.ToString(Console.ReadLine());
+
+            // USER AGE
             Console.WriteLine("Enter your Age");
             int age = Convert.ToInt32(Console.ReadLine());
+
+            // USER STUDENT NUMBER
             Console.WriteLine("Enter your Student Number");
-            string studentnumber = Convert.ToString(Console.ReadLine());
+            int studentnumber = Convert.ToInt32(Console.ReadLine());
+
+            // USER PASSWORD
             Console.WriteLine("Enter your Password");
             string password = Convert.ToString(Console.ReadLine());
+
+            // USER COURSE
+            Console.WriteLine("Enter your Course");
+            string course = Convert.ToString(Console.ReadLine());
+
+            // Create Student Object
+            Student student = new Student(name, age, studentnumber.ToString(), password, course);
+            student.DisplayInfo();
+            Console.WriteLine( "Please enter your password again to validate:");
+            string inputPassword = Convert.ToString(Console.ReadLine());
+
+
+
+
+
 
 
 
