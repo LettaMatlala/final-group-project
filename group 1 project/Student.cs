ad71361e98e1858bdf38e0eeb3f4e28dc6c421e6
Lee <Lettamatlala9@gmail.com>
2026-01-19 08:58:57 +0200

    line 29 to line 53 on classAdd project files.

---
+++ group 1 project/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace group_1_project
+{
+    internal class Student
+    {
+        // Properties of the Student
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string StudentNumber { get; set; }
+        public string Password { get; set; }
+        public string course { get; set; }
+
+        // Constructor
+        public Student(string name, int age, string studentnumber, string password, string course)
+        {
+            Name = name;
+            Age = age;
+            StudentNumber = studentnumber;
+            Password = password;
+            this.course = course;
+        }
+
+
+        // Generate correct password.       
+        // password == surname + last four numbers of your student number       
+        // Generate password based on name and last 4 digits of student number
+        // Generate correct password
+        private string GeneratePassword()
+        {
+            string last4 = StudentNumber.Substring(StudentNumber.Length - 4);
+            return Name + last4;
+        }
+
+        // Validate password
+        public bool ValidatePassword(string inputPassword)
+        {
+            string correctPassword = GeneratePassword();
+
+            if (inputPassword == correctPassword)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(" Access denied: Incorrect password");
+                return false;
+            }
+        }
+
+
+        // Method to display student information
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Student Number: {StudentNumber}");
+        }
+
+        //Method for student to enter password
+        public bool EnterPassword(string inputPassword, string correctPassword)
+        {
+            return inputPassword == correctPassword;
+        }
+
+    }
+}
