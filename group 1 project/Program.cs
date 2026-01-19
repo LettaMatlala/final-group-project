65e8d35101d3adbaca1771152dd5e66d230e0ba1
kakgiso idah <tsharanakakgiso@gmail.com>
2026-01-19 09:48:07 +0200

    C.W

--- group 1 project/Program.cs
+++ group 1 project/Program.cs
@@ -10,6 +10,23 @@ namespace group_1_project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your Name");
+
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Student Number");
+            string studentnumber = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your Password");
+            string password = Convert.ToString(Console.ReadLine());
+
+
+
+
+
         }
+
+
+
     }
 }
